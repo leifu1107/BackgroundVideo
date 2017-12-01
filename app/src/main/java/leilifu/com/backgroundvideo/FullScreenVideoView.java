c7c8c0719aff7d93269603632a73660f79271923
leilifu <844023833@qq.com>
2017-12-01 15:05:17 +0800

    1

--- app/src/main/java/leilifu/com/backgroundvideo/FullScreenVideoView.java
+++ app/src/main/java/leilifu/com/backgroundvideo/FullScreenVideoView.java
@@ -1,9 +1,7 @@
 package leilifu.com.backgroundvideo;
 
 import android.content.Context;
-import android.media.MediaPlayer;
 import android.util.AttributeSet;
-import android.view.KeyEvent;
 import android.widget.VideoView;
 
 /**
@@ -23,23 +21,6 @@ public class FullScreenVideoView extends VideoView {
         super(context, attrs, defStyleAttr);
     }
 
-    public FullScreenVideoView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
-    }
-
-    // region public
-
-    @Override
-    public void setOnPreparedListener(MediaPlayer.OnPreparedListener l)
-    {
-        super.setOnPreparedListener(l);
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event)
-    {
-        return super.onKeyDown(keyCode, event);
-    }
 
 
     // 实现全屏,重新计算高度和宽度
